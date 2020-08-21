d2aa29e8e2a4c792b32045c0bcea0b7c6ba615b8
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-08-21 18:55:32 +0400

    clenup unessery code

--- src/containers/TheHeader.js
+++ src/containers/TheHeader.js
@@ -16,12 +16,12 @@ import CIcon from '@coreui/icons-react'
 // routes config
 import routes from '../routes'
 
-import { 
-  TheHeaderDropdown,
-  TheHeaderDropdownMssg,
-  TheHeaderDropdownNotif,
-  TheHeaderDropdownTasks
-}  from './index'
+// import { 
+//   TheHeaderDropdown,
+//   TheHeaderDropdownMssg,
+//   TheHeaderDropdownNotif,
+//   TheHeaderDropdownTasks
+// }  from './index'
 
 const TheHeader = () => {
   const dispatch = useDispatch()
@@ -39,40 +39,42 @@ const TheHeader = () => {
 
   return (
     <CHeader withSubheader>
-      <CToggler
+      {/* <CToggler
         inHeader
         className="ml-md-3 d-lg-none"
         onClick={toggleSidebarMobile}
-      />
-      <CToggler
+      /> */}
+      {/* <CToggler
         inHeader
         className="ml-3 d-md-down-none"
         onClick={toggleSidebar}
-      />
-      <CHeaderBrand className="mx-auto d-lg-none" to="/">
-        <CIcon name="logo" height="48" alt="Logo"/>
-      </CHeaderBrand>
+      /> */}
 
-      <CHeaderNav className="d-md-down-none mr-auto">
+      {/* <CHeaderBrand className="mx-auto d-lg-none" to="/">
+        <CIcon name="logo" height="48" alt="Logo"/>
+      </CHeaderBrand> */}
+
+      {/* <CHeaderNav className="d-md-down-none mr-auto">
         <CHeaderNavItem className="px-3" >
           <CHeaderNavLink to="/dashboard">Dashboard</CHeaderNavLink>
         </CHeaderNavItem>
         <CHeaderNavItem  className="px-3">
           <CHeaderNavLink to="/users">Users</CHeaderNavLink>
         </CHeaderNavItem>
+        
         <CHeaderNavItem className="px-3">
           <CHeaderNavLink>Settings</CHeaderNavLink>
         </CHeaderNavItem>
-      </CHeaderNav>
+      </CHeaderNav> */}
 
-      <CHeaderNav className="px-3">
+      {/* <CHeaderNav className="px-3">
         <TheHeaderDropdownNotif/>
         <TheHeaderDropdownTasks/>
         <TheHeaderDropdownMssg/>
         <TheHeaderDropdown/>
-      </CHeaderNav>
+      </CHeaderNav> */}
 
-      <CSubheader className="px-3 justify-content-between">
+      {/* <CSubheader className="px-3 justify-content-between">
         <CBreadcrumbRouter 
           className="border-0 c-subheader-nav m-0 px-0 px-md-3" 
           routes={routes} 
@@ -92,7 +94,8 @@ const TheHeader = () => {
               <CIcon name="cil-settings" alt="Settings" />&nbsp;Settings
             </CLink>
           </div>
-      </CSubheader>
+      </CSubheader> */}
+
     </CHeader>
   )
 }
